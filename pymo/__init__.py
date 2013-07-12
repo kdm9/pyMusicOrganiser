071b0ee527833730a98189cd1f2c56c46b460642
Kevin Murray <k.d.murray.91@gmail.com>
2013-07-12 23:34:11 +1000

    fix a few bugs and implement remaining main features

--- pymo/__init__.py
+++ pymo/__init__.py
@@ -7,7 +7,6 @@ import re
 
 TAGS = ['album', 'comment', 'performer', 'title', 'track', 'year', 'genre']
 
-
 TAG_FORMATS = {
     'album': "%a",
     'performer': "%p",
@@ -19,16 +18,67 @@ TAG_FORMATS = {
 }
 
 
-def _get_new_filename_from_tags_and_format(tags, format_str):
+def _get_new_filename(tags, format_str, pad):
     filename = format_str
     for tag, fmt in TAG_FORMATS.items():
         if tags[tag]:
             tag_value = tags[tag]
+            if tag == "track":
+                tag_value.zfill(pad)
             filename = filename.replace(fmt, tag_value)
     return filename
 
 
+def _transform_filename(filename, transforms):
+    new_filename = filename
+    if "lc" in transforms:
+        new_filename = new_filename.lower()
+    elif "uc" in transforms:
+        new_filename = new_filename.upper()
+    elif "jc" in transforms:
+        segments = []  # to be joined with os.path.join
+        for segment in os.path.split(new_filename):
+            words = segment.split()
+            segment = words.pop(0).lower()  # first one is lowercase
+            for word in words:
+                word = word.lower()
+                segment += word[0].upper()
+                segment += word[1:].lower()
+            segments.append(segment)
+        new_filename = os.path.join(*segments)
+    elif "cc" in transforms:
+        segments = []  # to be joined with os.path.join
+        for segment in os.path.split(new_filename):
+            words = segment.split()
+            segment = ""
+            for word in words:
+                word = word.lower()
+                segment += word[0].upper()
+                segment += word[1:].lower()
+            segments.append(segment)
+        new_filename = os.path.join(*segments)
+
+    if "us" in transforms:
+        new_filename = new_filename.replace(" ", "_")
+
+    if "rs" in transforms:
+        new_filename = new_filename.replace(" ", "")
+
+    if "rp" in transforms:
+        new_filename = re.sub("[!@#$%^&*():;?=+~`<>,]", "", new_filename)
+
+    if "pu" in transforms:
+        new_filename = re.sub("[!@#$%^&*():;?=+~`<>,]", "_", new_filename)
+
+    return new_filename
+
+
 def _get_file_tags(filename):
+    """
+    Given a filename, this retreives all tags it can, and returns a dict.
+    If a tag is unavailable, ``None`` is given as it's value.
+    """
+    #TODO: use mutagen, for cross-format tag retreival
     id3tags = id3reader.Reader(filename)
     tags = {}
     for tag in TAGS:
@@ -54,7 +104,12 @@ def _walk(rootdir, excludedirs=[]):
 def main(opts):
     indir = opts["<inputdir>"]
     outdir = opts["<outputdir>"]
-    fmt = opts["<format>"]
+
+    if opts["-f"]:
+        fmt = opts["-p"]
+    else:
+        fmt = "%p/%a/%t-%n"
+
     if opts["-m"]:
         method = "move"
         cmd = "mv"
@@ -65,11 +120,22 @@ def main(opts):
         method = "copy"
         cmd = "cp"
 
+    if opts["-p"]:
+        pad = int(opts["-p"])
+    else:
+        pad = 2
+
+    if opts["-t"]:
+        transforms = opts["TRANSFORMS"].split(",")
+    else:
+        transforms = []
+
     soundfiles = _walk(indir, [outdir,])
     for soundfile in soundfiles:
         soundfile_tags = _get_file_tags(soundfile)
-        newfile = _get_new_filename_from_tags_and_format(soundfile_tags, fmt)
-        if "%" in newfile:
+        newfile = _get_new_filename(soundfile_tags, fmt, pad)
+        newfile = _transform_filename(newfile, transforms)
+        if "%" in newfile:  # a tag has not been replaced
             sys.stderr.write("Couldn't get tags required to move '%s'\n" % \
                     soundfile)
             continue
@@ -87,6 +153,7 @@ def main(opts):
                             newdirname)
                     continue
 
+            # do the move/cp/link
             try:
                 if method == "move":
                     os.rename(soundfile, newfile)
--- pymo/__main__.py
+++ pymo/__main__.py
@@ -1,2 +1,38 @@
+from docopt import docopt
 from pymo import main
-main()
+
+
+_PROGRAM_ARGS = """
+USAGE:
+    pymo [-f FORMAT -p PAD -v -n (-l|-c|-m) -t TRANSFORMS] <inputdir> <outputdir>
+OPTIONS:
+    -f FORMAT       Format. Default is "%p/%a/%t-%n", where:
+                        %n:  Song title
+                        %t:  Track number
+                        %a:  Album
+                        %p:  Performer
+                        %g:  Genre
+                        %c:  Comment
+                        %y:  Release year
+    -t TRANSFORMS   Text Transforms. A comma seperated list of any of the
+                    following:
+                        lc: lower case
+                        uc: UPPER CASE
+                        cc: CamelCase (removes spaces)
+                        jc: javaCase (removes spaces)
+                        us: Replace spaces with underscores
+                        rs: Remove spaces
+                        rp: Remove punctuation
+                            [any of: !@#$%^&*():;?=+~`<>, ]
+                        pu: Convert punctuation to underscore
+    -p PAD          Width of padding (0 = Don't pad) [DEFAULT: 2]
+    -v              Be verbose (prints move/link/copy commands)
+    -n              Do a dry run (just print which files would be moved)
+    -m              Move files (DEFAULT)
+    -c              Copy files
+    -l              Symlink files
+"""
+
+
+opts = docopt(_PROGRAM_ARGS)
+main(opts)
